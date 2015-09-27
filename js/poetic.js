0ed09e32fcaf5503145feecdebd57ae3ced7a2dc
Nathan Brown <nkabrown@gmail.com>
2015-09-27 12:41:46 -0400

    rearrange letters
    Write functions to translate letters along the x-axis.

--- js/poetic.js
+++ js/poetic.js
@@ -1,4 +1,5 @@
 var characters = ['d', 'e', 'm', 'o', 's'];
+var time = 9000;
 
 var letters = d3.select('.letters').selectAll('.letter')
     .data(characters)
@@ -7,9 +8,23 @@ var letters = d3.select('.letters').selectAll('.letter')
     .text(function(d) { return d; });
 
 var letterD = letters.filter(function(d) { return d == 'd'; });
+var letterE = letters.filter(function(d) { return d == 'e'; });
+var letterM = letters.filter(function(d) { return d == 'm'; });
+var letterO = letters.filter(function(d) { return d == 'o'; });
 
 rotation(letterD);
+moveRight(letterE);
+moveRight(letterM);
+moveLeft(letterO);
 
 function rotation(selection) {
-  selection.transition('flip').duration(4000).each('start', function() { d3.select(this).style('transform', 'rotate(0deg)'); }).style('transform', 'rotate(180deg) translateY(-25px)');
+  selection.transition('flip').duration(time).each('start', function() { d3.select(this).style('transform', 'rotate(0deg)'); }).style('transform', 'rotate(-180deg) translateY(-25px)');
+}
+
+function moveRight(selection) {
+  selection.transition().duration(time).each('start', function() { d3.select(this).style('transform', 'translateX(0)') }).style('transform', 'translateX(100px)');
+}
+
+function moveLeft(selection) {
+  selection.transition().duration(time).each('start', function() { d3.select(this).style('transform', 'translateX(0)') }).style('transform', 'translateX(-215px)');
 }
