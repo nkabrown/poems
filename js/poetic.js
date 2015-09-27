53b9ebee9965417145f97b83040e562deb16c2e3
Nathan Brown <nkabrown@gmail.com>
2015-09-27 12:12:13 -0400

    rotate letter d into a p
    Write a function to handle the rotation and translation of letters.

--- js/poetic.js
+++ js/poetic.js
@@ -1,8 +1,15 @@
 var characters = ['d', 'e', 'm', 'o', 's'];
 
-d3.select('.letters').selectAll('.letter')
+var letters = d3.select('.letters').selectAll('.letter')
     .data(characters)
   .enter().append('div')
     .attr('class', 'letter')
-    .attr('data-index', function(d, i) { return i; })
     .text(function(d) { return d; });
+
+var letterD = letters.filter(function(d) { return d == 'd'; });
+
+rotation(letterD);
+
+function rotation(selection) {
+  selection.transition('flip').duration(4000).each('start', function() { d3.select(this).style('transform', 'rotate(0deg)'); }).style('transform', 'rotate(180deg) translateY(-25px)');
+}
